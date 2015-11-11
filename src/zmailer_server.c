fb532de2cf0b7e7aa867eb7cfe4a1c85377d8cb6
oikosdev <cb@oikosdev.net>
2015-11-11 15:32:53 +0100

    Problem: curl not integrated, Solution: done

--- src/zmailer_server.c
+++ src/zmailer_server.c
@@ -48,6 +48,12 @@ struct _client_t {
     //  TODO: Add specific properties for your application
 };
 
+struct upload_status {
+ int lines_read;
+ zmailer_msg_t *msg;
+};
+
+
 //  Include the generated server engine
 #include "zmailer_server_engine.inc"
 
@@ -76,7 +82,6 @@ server_method (server_t *self, const char *method, zmsg_t *msg)
 {
     return NULL;
 }
-
 
 //  Allocate properties and structures for a new client connection and
 //  optionally engine_set_next_event (). Return 0 if OK, or -1 on error.
@@ -130,7 +135,7 @@ zmailer_server_test (bool verbose)
     zmailer_msg_t *request2 = zmailer_msg_new ();
     zmailer_msg_set_id (request2, ZMAILER_MSG_MAIL);
     zmailer_msg_set_from (request2, "oikosdev <cb@oikosdev.net>");
-    zmailer_msg_set_to   (request2, "oikosdev <cb@oikosdev.net>");
+    zmailer_msg_set_to   (request2, "<christophe.beauce@gmail.com>");
     zmailer_msg_set_subject (request2, "Test");
     zmailer_msg_set_request (request2, "This is a test email for you !");
     zmailer_msg_send (request2, client);
@@ -163,7 +168,98 @@ echo (client_t *self)
 static void
 send_email (client_t *self)
 {
+
+ assert (self);
+ assert (self->message);
  zsys_info ("sending email !");
- const char *subject = zmailer_msg_subject (self->message);
- zsys_info ("received subject: %s", subject);
+ sleep (2);
+
+ CURL *curl;
+ CURLcode res = CURLE_OK;
+ struct curl_slist *recipients = NULL;
+
+ FILE *pFile = fopen ("./data/mail.txt","w");
+ // todo
+ fprintf (pFile,"Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n");
+ zsys_info ("To: %s \r\n", zmailer_msg_to(self->message));
+ fprintf (pFile,"To: %s\r\n", zmailer_msg_to(self->message));
+ fprintf (pFile, "From: %s\r\n", zmailer_msg_from (self->message));
+ fprintf (pFile, "Subject: %s \r\n", zmailer_msg_subject (self->message));
+ fprintf (pFile, "\r\n");
+ fprintf (pFile, "%s Thank you. \r\n", zmailer_msg_request(self->message));
+ // fprintf (pFile, "\r\n.\r\n");
+ fclose (pFile);
+ sleep (20);
+
+// zfile_t* zf2 = zfile_new ("data","mail.txt");
+// zfile_input (zf2);
+
+ FILE * pMail;
+ pMail = fopen ("./data/mail.txt","r");
+// = zfile_handle (zf2);
+
+ curl = curl_easy_init();
+ if(curl) {
+    /* Set username and password */ 
+    curl_easy_setopt(curl, CURLOPT_USERNAME, zconfig_get (self->server->config, "zmailer_server/user", ""));
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, zconfig_get (self->server->config, "zmailer_server/password", ""));
+ 
+    /* This is the URL for your mailserver. Note the use of port 587 here,
+     * instead of the normal SMTP port (25). Port 587 is commonly used for
+     * secure mail submission (see RFC4403), but you should use whatever
+     * matches your server configuration. */ 
+    curl_easy_setopt(curl, CURLOPT_URL, zconfig_get (self->server->config, "zmailer_server/server", ""));
+
+    /* In this example, we'll start with a plain text connection, and upgrade
+     * to Transport Layer Security (TLS) using the STARTTLS command. Be careful
+     * of using CURLUSESSL_TRY here, because if TLS upgrade fails, the transfer
+     * will continue anyway - see the security discussion in the libcurl
+     * tutorial for more details. */ 
+    curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
+ 
+    /* If your server doesn't have a valid certificate, then you can disable
+     * part of the Transport Layer Security protection by setting the
+     * CURLOPT_SSL_VERIFYPEER and CURLOPT_SSL_VERIFYHOST options to 0 (false).*/
+     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); 
+    /* Note that this option isn't strictly required, omitting it will result in
+     * libcurl sending the MAIL FROM command with empty sender data. All
+     * autoresponses should have an empty reverse-path, and should be directed
+     * to the address in the reverse-path which triggered them. Otherwise, they
+     * could cause an endless loop. See RFC 5321 Section 4.5.5 for more details.
+     */ 
+    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, zmailer_msg_from (self->message));
+
+    /* Add two recipients, in this particular case they correspond to the
+     * To: and Cc: addressees in the header, but they could be any kind of
+     * recipient. */ 
+    recipients = curl_slist_append(recipients, zmailer_msg_to (self->message));
+    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+ 
+    /* We're using a callback function to specify the payload (the headers and
+     * body of the message). You could just use the CURLOPT_READDATA option to
+     * specify a FILE pointer to read from. */ 
+    curl_easy_setopt(curl, CURLOPT_READDATA, pMail);
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+ 
+    /* Since the traffic will be encrypted, it is very useful to turn on debug
+     * information within libcurl to see what is happening during the transfer.
+     */ 
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+ 
+    /* Send the message */ 
+    res = curl_easy_perform(curl);
+ 
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* Free the list of recipients */ 
+    curl_slist_free_all(recipients);
+ 
+    /* Always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+assert(pMail); 
 }
